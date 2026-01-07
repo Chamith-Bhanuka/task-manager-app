584ee7e8b5e083748deb937655811bca226688d4
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2026-01-07 10:23:05 +0530

    feat(auth): enable React Native persistence with AsyncStorage in firebase auth initialization

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
-import { initializeAuth } from "@firebase/auth";
+// @ts-ignore
+import { getReactNativePersistence, initializeAuth} from "@firebase/auth";
 import { getFirestore } from "@firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import dotenv from "dotenv";
 
@@ -17,6 +19,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = initializeAuth(app);
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)
+});
 
 export const db = getFirestore(app)
