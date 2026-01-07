d261435187ae6e865f37210939bc2619f0452569
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2026-01-07 13:50:57 +0530

    chore: include context directory in Tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: [
-    "./App.tsx",
-    "./components/**/*.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}"
+    './App.tsx',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './app/**/*.{js,jsx,ts,tsx}',
+    './context/**/*.{js,jsx,ts,tsx}',
   ],
-  presets: [require("nativewind/preset")],
+  presets: [require('nativewind/preset')],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
