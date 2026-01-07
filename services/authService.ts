ae809f0c88c44d9d501ed5333e38222403f165e2
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2026-01-07 10:48:09 +0530

    feat(auth): add authService with registerUser function to create user, update profile, and store in Firestore

---
+++ services/authService.ts
@@ -0,0 +1,25 @@
+import { createUserWithEmailAndPassword, updateProfile } from '@firebase/auth';
+import { auth, db } from './firebase';
+import { doc, setDoc } from '@firebase/firestore';
+
+export const registerUser = async (
+  fullName: string,
+  email: string,
+  password: string
+) => {
+  const userCredential = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+  await updateProfile(userCredential.user, { displayName: fullName });
+  await setDoc(doc(db, 'users', userCredential.user.uid), {
+    name: fullName,
+    role: '',
+    email,
+    createAt: new Date(),
+  });
+  return userCredential.user;
+};
+
+export const login = async () => {};
