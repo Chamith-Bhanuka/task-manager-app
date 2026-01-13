eeac2e1e575ec73e68a7bf5e349c718503249e35
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2026-01-13 23:06:49 +0530

    feat(auth): add global styles and redirect logic to Index based on auth state

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,24 @@
-import "../global.css";
-import {Redirect} from "expo-router";
+import '../global.css';
+import { Redirect } from 'expo-router';
+import { useAuth } from '@/hooks/useAuth';
+import { ActivityIndicator, View } from 'react-native';
 
 const Index = () => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <View className="flex-1 justify-center items-center bg-gray-50">
+        <ActivityIndicator size="large" color="blue" />
+      </View>
+    );
+  }
+
+  if (user) {
+    return <Redirect href="/home" />;
+  } else {
     return <Redirect href="/login" />;
+  }
 };
 
 export default Index;
